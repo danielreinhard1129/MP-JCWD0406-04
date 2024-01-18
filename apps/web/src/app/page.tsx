36fabcfc1a1a0ca6fc37cecca4df7a8855ca15e3
AnggrainiAng <anggi3296@gmail.com>
2024-01-18 13:58:36 +0700

    feat: add fitur login

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,11 +3,5 @@ import styles from './page.module.css';
 import { Button } from 'flowbite-react';
 
 export default function Home() {
-  return (
-    <main className="container max-w-7xl px-4 mx-auto">
-      <section className="mt-20 flex flex-col items-center justify-center text-center md:mt-32">
-        <h1 className="mb-4 text-4xl font-semibold md:text-4xl">Homepage</h1>
-      </section>
-    </main>
-  );
+  return <main className="container max-w-7xl px-4 mx-auto"></main>;
 }
